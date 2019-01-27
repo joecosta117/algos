f235bbd16d5018c47fab7083c7e6ab386af89397
joecosta117 <joecosta117@gmail.com>
2019-01-27 18:47:51 -0500

    created algo folder and put in description for first algo

---
+++ str-algos/underscorify-substring.js
@@ -0,0 +1,8 @@
+//Underscorify Substring
+//takes in a string and substring.
+//returns version of string with every instance of substring wrapped
+//in underscores. if two substrings overlap each other, then underscores should
+//only appear on the left and right of the combined substrings.
+//if there are no substrings, return the original string
+//ex: underscorify("testthis is a testtest to see if testestest works")
+//expected: "_test_this is a _testtest_ to see if _testestest_ works"
