3d588c3279d6b81ecbdc8c23750c5c2fd48f9900
joecosta117 <joecosta117@gmail.com>
2019-03-09 13:31:55 -0500

    insertion sort v2

--- str-algos/insertion-sort.js
+++ str-algos/insertion-sort.js
@@ -1,5 +1,20 @@
 function insertionSort(array) {
   // Write your code here.
+  for (let i = 0; i < array.length; i++) {
+    let j = i
+    while (j > 0 && array[j] < array[j - 1]) {
+      let prev = array[j - 1]
+      array[j - 1] = array[j]
+      array[j] = prev
+      j--
+    }
+  }
+
+  return array
+}
+
+function insertionSort(array) {
+  // Write your code here.
   let sortArr = [array[0]]
 
   for (let i = 1; i < array.length; i++) {
