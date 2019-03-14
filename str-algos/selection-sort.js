b0589ae821238fce30529bc2ced25fd4a2df8e3b
joecosta117 <joecosta117@gmail.com>
2019-03-14 11:54:24 -0400

    addign selection sort

--- str-algos/selection-sort.js
+++ str-algos/selection-sort.js
@@ -1,3 +1,24 @@
 function selectionSort(array) {
   // Write your code here.
+  let curMin;
+  let curItem;
+  let curMinIdx;
+
+  for (let i = 0; i < array.length; i++) {
+    curMin = array[i]
+    curItem = array[i]
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < curMin) {
+        curMin = array[j]
+        curMinIdx = j
+      }
+      if (j === array.length - 1 && curMin !== curItem) {
+        array[i] = curMin
+        array[curMinIdx] = curItem
+      }
+    }
+  }
+  return array
 }
+
